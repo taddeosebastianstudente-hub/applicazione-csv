e77f5896a16e32039fbeb30b064f508578a3caf9
taddeosebastianstudente-hub <taddeo.sebastian.studente@itispaleocapa.it>
2026-05-08 10:54:15 +0200

    Add files via upload

--- esercizio_info.cpp
+++ esercizio_info.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-strcut Info{
+struct Info{
 	string ClasseToponimo;
 	string DescrizioneToponimo;
 	int Numero;
@@ -12,7 +12,20 @@ strcut Info{
 	double Lon;
 	double Location;
 	};
+void CaricaDati(Info x[], string y){
+	ifstream fileInput("testo.txt");
+    string linea;
+    if (fileInput.is_open()) {
+        while (fileInput>>linea){
+            cout<<linea<<'\n';
+        }
+        fileInput.close();
+    } else {
+        cout << "Impossibile aprire il file";
+    }
+	}
+
 int main(int argc, char** argv) {
 	
 	return 0;
-}
+}    //prendere il file, prendere il contenuto e metterli qua
