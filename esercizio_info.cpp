a2fe906ddd1e5b40bda6df1a276ce3f71bfc344b
taddeosebastianstudente-hub <taddeo.sebastian.studente@itispaleocapa.it>
2026-05-08 10:31:17 +0200

    Add files via upload

---
+++ esercizio_info.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+strcut Info{
+	string ClasseToponimo;
+	string DescrizioneToponimo;
+	int Numero;
+	string Subalterno;
+	int CAP;
+	int SezioneISTAT;
+	double Lat;
+	double Lon;
+	double Location;
+	};
+int main(int argc, char** argv) {
+	
+	return 0;
+}
